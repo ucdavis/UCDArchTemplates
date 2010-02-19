869939e1682ef09c10b5025be2f1a6f6e0bab59b
Scott Kirkland <srkirkland@gmail.com>
2010-02-19 13:47:51 -0800

    Removed the explicit reference to the local models to make it more generic

--- UCDArchTemplates/Controllers/CustomerController.cs
+++ UCDArchTemplates/Controllers/CustomerController.cs
@@ -1,18 +1,177 @@
-﻿using System.Web.Mvc;
+﻿using System;
+using System.Web.Mvc;
+using UCDArch.Core.PersistanceSupport;
+using UCDArch.Web.Controller;
+using UCDArch.Web.Helpers;
+using UCDArch.Core.Utils;
 using UCDArchTemplates.Models;
 
 namespace UCDArchTemplates.Controllers
 {
-    public class CustomerController : Controller
+    /// <summary>
+    /// Controller for the Customer class
+    /// </summary>
+    public class CustomerController : SuperController
     {
+	    private readonly IRepository<Customer> _customerRepository;
+
+        public CustomerController(IRepository<Customer> customerRepository)
+        {
+            _customerRepository = customerRepository;
+        }
+    
         //
         // GET: /Customer/
         public ActionResult Index()
         {
-            var customers = Customer.GetAll();
+            var customerList = _customerRepository.Queryable;
 
-            return View(customers);
+            return View(customerList);
+        }
+
+        //
+        // GET: /Customer/Details/5
+        public ActionResult Details(int id)
+        {
+            var customer = _customerRepository.GetNullableByID(id);
+
+            if (customer == null) return RedirectToAction("Index");
+
+            return View(customer);
+        }
+
+        //
+        // GET: /Customer/Create
+        public ActionResult Create()
+        {
+			var viewModel = CustomerViewModel.Create(Repository);
+            
+            return View(viewModel);
+        } 
+
+        //
+        // POST: /Customer/Create
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Create(Customer customer)
+        {
+            var customerToCreate = new Customer();
+
+            TransferValues(customer, customerToCreate);
+
+            customerToCreate.TransferValidationMessagesTo(ModelState);
+
+            if (ModelState.IsValid)
+            {
+                _customerRepository.EnsurePersistent(customerToCreate);
+
+                Message = "Customer Created Successfully";
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+				var viewModel = CustomerViewModel.Create(Repository);
+                viewModel.Customer = customer;
+
+                return View(viewModel);
+            }
+        }
+
+        //
+        // GET: /Customer/Edit/5
+        public ActionResult Edit(int id)
+        {
+            var customer = _customerRepository.GetNullableByID(id);
+
+            if (customer == null) return RedirectToAction("Index");
+
+			var viewModel = CustomerViewModel.Create(Repository);
+			viewModel.Customer = customer;
+
+			return View(viewModel);
+        }
+        
+        //
+        // POST: /Customer/Edit/5
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Edit(int id, Customer customer)
+        {
+            var customerToEdit = _customerRepository.GetNullableByID(id);
+
+            if (customerToEdit == null) return RedirectToAction("Index");
+
+            TransferValues(customer, customerToEdit);
+
+            customerToEdit.TransferValidationMessagesTo(ModelState);
+
+            if (ModelState.IsValid)
+            {
+                _customerRepository.EnsurePersistent(customerToEdit);
+
+                Message = "Customer Edited Successfully";
+
+                return RedirectToAction("Index");
+            }
+            else
+            {
+				var viewModel = CustomerViewModel.Create(Repository);
+                viewModel.Customer = customer;
+
+                return View(viewModel);
+            }
+        }
+        
+        //
+        // GET: /Customer/Delete/5 
+        public ActionResult Delete(int id)
+        {
+			var customer = _customerRepository.GetNullableByID(id);
+
+            if (customer == null) return RedirectToAction("Index");
+
+            return View(customer);
+        }
+
+        //
+        // POST: /Customer/Delete/5
+        [AcceptVerbs(HttpVerbs.Post)]
+        public ActionResult Delete(int id, Order customer)
+        {
+			var customerToDelete = _customerRepository.GetNullableByID(id);
+
+            if (customerToDelete == null) return RedirectToAction("Index");
+
+            _customerRepository.Remove(customerToDelete);
+
+            Message = "Customer Removed Successfully";
+
+            return RedirectToAction("Index");
+        }
+        
+        /// <summary>
+        /// Transfer editable values from source to destination
+        /// </summary>
+        private static void TransferValues(Customer source, Customer destination)
+        {
+            throw new NotImplementedException();
         }
 
     }
+
+	/// <summary>
+    /// ViewModel for the Customer class
+    /// </summary>
+    public class CustomerViewModel
+	{
+		public Customer Customer { get; set; }
+ 
+		public static CustomerViewModel Create(IRepository repository)
+		{
+			Check.Require(repository != null, "Repository must be supplied");
+			
+			var viewModel = new CustomerViewModel {Customer = new Customer()};
+ 
+			return viewModel;
+		}
+	}
 }
